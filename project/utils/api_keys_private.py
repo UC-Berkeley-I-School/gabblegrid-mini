c71258a7929054c8488284d53d6d94c1b66e9083
gaurav8936 <gaurav.narasimhan@berkeley.edu>
2024-09-21 18:04:08 -0700

    Delete project/utils/api_keys_private.py

--- project/utils/api_keys_private.py
+++
@@ -1,8 +0,0 @@
-# File: api_keys_private.py
-
-openai_api_key = "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"
-openweather_api_key = "864a9bb0b562e1c87e01b38880d5bee7"
-bing_api_key = "7e534482ac5f46d3a2a979072b19e591" 
-# api_keys_private.py
-
-GMAIL_APP_PASSWORD = "ikds tqco rjth eqqw"  # Replace with the app-specific password
